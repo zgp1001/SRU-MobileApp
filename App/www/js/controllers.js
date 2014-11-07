126652f77ed60719a585e1ca157325e810fa6c6e
DJM1025 <djm1025@sru.edu>
2014-11-07 13:30:30 -0500

    Added the rest of the safety tabs
    Still need to make them all work

--- App/www/js/controllers.js
+++ App/www/js/controllers.js
@@ -34,7 +34,26 @@ angular.module('starter.controllers', [])
 		{itemName: "Angry Person", itemState: "/app/safety"},
 		{itemName: "Behaviors of Concern", itemState: "/app/safety"},
 		{itemName: "Communication Tips", itemState: "/app/safety"},
-		{itemName: "Referral Sources/Consultants", itemState: "/app/safety"}
+		{itemName: "Referral Sources/Consultants", itemState: "/app/safety"},
+		{itemName: "De-Escalation Skills", itemState: "/app/safety"},
+		{itemName: "Disruptive Student", itemState: "/app/safety"},
+		{itemName: "Emotional Crisis", itemState: "/app/safety"},
+		{itemName: "Helpful Comments", itemState: "/app/safety"},
+		{itemName: "Financially Troubled", itemState: "/app/safety"},
+		{itemName: "How to make a student conduct referral", itemState: "/app/safety"},
+		{itemName: "Medical Emergency", itemState: "/app/safety"},
+		{itemName: "Mental Health Emergency", itemState: "/app/safety"},
+		{itemName: "Physical Signs of Concern", itemState: "/app/safety"},
+		{itemName: "Sexual Harassment/Stalking/Rape/Assault", itemState: "/app/safety"},
+		{itemName: "Student in Crisis", itemState: "/app/safety"},
+		{itemName: "Substance Abuse", itemState: "/app/safety"},
+		{itemName: "Suicidal Thoughts/Ideations", itemState: "/app/safety"},
+		{itemName: "Threat Assessment Team", itemState: "/app/safety"},
+		{itemName: "Who should be referred", itemState: "/app/safety"},
+		{itemName: "Victim/Survivor Assistance", itemState: "/app/safety"},
+		{itemName: "Violent Person", itemState: "/app/safety"},
+		{itemName: "Websites", itemState: "/app/safety"},
+		{itemName: "BIT Membership", itemState: "/app/safety"}
 	];
 })
 .controller('DiningCtrl',function($scope){
