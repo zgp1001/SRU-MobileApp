b79f56bb2937bc34892cd9b0b75dd55105fd2dc1
zgp1001 <zacharypetrusch@gmail.com>
2015-01-08 14:08:33 -0500

    Maps page shows map now
    Starting location is the campus

--- App/www/js/controllers.js
+++ App/www/js/controllers.js
@@ -12,20 +12,27 @@ angular.module('starter.controllers', [])
 
 })
 
-.controller('MapCtrl', function($scope){
-  google.maps.event.addDomListener(window, 'load', function() {
-        var myLatlng = new google.maps.LatLng(37.3000, -120.4833);
+.controller('MapCtrl', function($scope, $ionicLoading){
+  	var myLatlng = new google.maps.LatLng(41.062812, -80.042031);
  
-        var mapOptions = {
-            center: myLatlng,
-            zoom: 16,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
- 
-        var map = new google.maps.Map(document.getElementById("map"), mapOptions);
- 
-        $scope.map = map;
-})
+	var mapOptions = {
+	    center: myLatlng,
+	    zoom: 16,
+	    mapTypeId: google.maps.MapTypeId.SATELLITE  //Make .ROADMAP for just map view
+	};
+
+	var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+	navigator.geolocation.getCurrentPosition(function(pos) {
+	    map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+	    var myLocation = new google.maps.Marker({
+	        position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
+	        map: map,
+	        title: "My Location"
+	    });
+	});
+
+	$scope.map = map;
 })
 
 .controller('SafetyCtrl',function($scope){
